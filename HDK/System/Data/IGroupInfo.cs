8e9e9ca9c8459e859f34e80ec84d87c6354c2244
HeToC <hetoc-la-croix@hotmail.com>
2013-09-17 18:19:00 +0400

    111

---
+++ HDK/System/Data/IGroupInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace System.Data
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TElement"></typeparam>
+    public interface IGroupInfo<TKey, TElement> : IGrouping<TKey, TElement>, ICollectionViewGroup
+    {
+    }
+}
