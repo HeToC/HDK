1c7639c8d3226e5e2708b46c78efc2330a6b32a1
HeToC <hetoc-la-croix@hotmail.com>
2013-09-19 17:58:11 +0400

    1

---
+++ HDK/System/Collections/Generic/VectorChangedEventArgs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// Class that implements IVectorChangedEventArgs so we can fire VectorChanged events.
+    /// </summary>
+    public class VectorChangedEventArgs : IVectorChangedEventArgs
+    {
+        static VectorChangedEventArgs _reset = new VectorChangedEventArgs(CollectionChange.Reset);
+
+        CollectionChange _cc = CollectionChange.Reset;
+        uint _index = (uint)0xffff;
+
+        public static VectorChangedEventArgs Reset
+        {
+            get { return _reset; }
+        }
+
+        public VectorChangedEventArgs(CollectionChange cc, int index = -1, object item = null)
+        {
+            _cc = cc;
+            _index = (uint)index;
+        }
+
+        public CollectionChange CollectionChange
+        {
+            get { return _cc; }
+        }
+
+        public uint Index
+        {
+            get { return _index; }
+        }
+    }
+}
