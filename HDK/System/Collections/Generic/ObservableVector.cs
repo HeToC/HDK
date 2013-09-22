5cd5ae366d8115efd97637898cdbd8b4cdccd225
HeToC <hetoc-la-croix@hotmail.com>
2013-09-22 15:01:57 +0400

    ObservableVector

--- HDK/System/Collections/Generic/ObservableVector.cs
+++ HDK/System/Collections/Generic/ObservableVector.cs
@@ -6,62 +6,138 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Data;
 
 namespace System.Collections.Generic
 {
-    /// <summary>
-    /// IObservableVector<T> implementation.
-    /// </summary>
-    public class ObservableVector<T> : Collection<T>, INotifyPropertyChanged, IObservableVector<T>
+    public class ObservableVector<TElement> : ObservableVector<TElement, List<object>>
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        public event VectorChangedEventHandler<T> VectorChanged;
+    }
 
-        public ObservableVector() : base()
+    public class ObservableVector<TElement, TInner> :BindableBase, IObservableVector<TElement> where TInner: IList, new()
+    {
+        private TInner inner;
+
+        public ObservableVector()
         {
+            inner = InitializeInnerContainer();
         }
 
-        public ObservableVector(IList<T> list) : base(list)
+        protected virtual TInner InitializeInnerContainer()
         {
+            var tmp = (TInner)(new List<TElement>() as IList);
+            return tmp;
         }
 
-        protected override void ClearItems()
+        public event VectorChangedEventHandler<TElement> VectorChanged;
+        protected virtual void RaiseVectorChanged(CollectionChange collectionChange, int index = -1, TElement item = default(TElement))
         {
-            base.ClearItems();
-            this.RaisePropertyChanged(PropertyChanged, "Count", "Items");
+            var handler = this.VectorChanged;
+            if (handler != null)
+                handler(this, new VectorChangedEventArgs(collectionChange,index));
+        }
+
+        public virtual int IndexOf(TElement item)
+        {
+            return this.inner.IndexOf(item);
+        }
+
+        public virtual void Insert(int index, TElement item)
+        {
+            CheckReadOnly();
+            this.inner.Insert(index, item);
+            RaisePropertyChanged("Items");
+            this.RaiseVectorChanged(CollectionChange.ItemInserted, index);
+        }
+
+        public virtual void RemoveAt(int index)
+        {
+            CheckReadOnly();
+            this.inner.RemoveAt(index);
+            RaisePropertyChanged("Count", "Items");
+            this.RaiseVectorChanged(CollectionChange.ItemRemoved, index);
+        }
+
+        public virtual TElement this[int index]
+        {
+            get
+            {
+                return (TElement)this.inner[index];
+            }
+            set
+            {
+                CheckReadOnly();
+                this.inner[index] = value;
+                this.RaisePropertyChanged("Items");
+                this.RaiseVectorChanged(CollectionChange.ItemChanged, index);
+            }
+        }
+
+        public virtual void Add(TElement item)
+        {
+            CheckReadOnly();
+            this.inner.Add(item);
+            this.RaisePropertyChanged("Count", "Items");
+            this.RaiseVectorChanged(CollectionChange.ItemInserted, this.inner.Count - 1);
+        }
+
+        public virtual void Clear()
+        {
+            CheckReadOnly();
+            this.inner.Clear();
+            this.RaisePropertyChanged("Count", "Items");
             this.RaiseVectorChanged(CollectionChange.Reset, 0);
         }
 
-        protected override void InsertItem(int index, T item)
+        public virtual bool Contains(TElement item)
         {
-            base.InsertItem(index, item);
-            this.RaisePropertyChanged(PropertyChanged, "Count", "Items");
-            this.RaiseVectorChanged(CollectionChange.ItemInserted, (uint)index);
+            return this.inner.Contains(item);
         }
 
-        protected override void RemoveItem(int index)
+        public virtual void CopyTo(TElement[] array, int arrayIndex)
         {
-            base.RemoveItem(index);
-            this.RaisePropertyChanged(PropertyChanged, "Count", "Items");
-            this.RaiseVectorChanged(CollectionChange.ItemRemoved, (uint)index);
+            this.inner.CopyTo(array, arrayIndex);
         }
 
-        protected override void SetItem(int index, T item)
+        public virtual int Count
         {
-            base.SetItem(index, item);
-            this.RaisePropertyChanged(PropertyChanged, "Items");
-            this.RaiseVectorChanged(CollectionChange.ItemChanged, (uint)index);
+            get { return this.inner.Count; }
         }
 
-        protected void RaiseVectorChanged(CollectionChange collectionChange, uint index)
+        public virtual bool IsReadOnly
         {
-            this.OnVectorChanged(new VectorChangedEventArgs(collectionChange, index));
+            get { return false; } // return inner.IsReadOnly; }
         }
 
-        protected virtual void OnVectorChanged(IVectorChangedEventArgs e)
+        public virtual bool Remove(TElement item)
         {
-            if (this.VectorChanged != null)
-                this.VectorChanged(this, e);
+            CheckReadOnly();
+
+            var index = this.inner.IndexOf(item);
+            if (index == -1)
+            {
+                return false;
+            }
+            this.RemoveAt(index);
+            return true;
+        }
+
+        public IEnumerator<TElement> GetEnumerator()
+        {
+            return this.inner.GetEnumerator() as IEnumerator<TElement>;
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.inner.GetEnumerator();
+        }
+
+        void CheckReadOnly()
+        {
+            if (IsReadOnly)
+            {
+                throw new Exception("The source collection cannot be modified.");
+            }
         }
     }
 }
---
+++ HDK/System/Collections/Generic/ObservableVectorView.cs
@@ -0,0 +1,147 @@
+﻿using Windows.UI.Xaml.Data;
+
+namespace System.Collections.Generic
+{
+    public class ObservableVectorView<TElement> : ObservableVectorView<TElement, List<TElement>>
+    {
+    }
+
+    public class ObservableVectorView<TElement, TInner> : ObservableVector<TElement, TInner> 
+        where TInner : IList, new()
+    {
+        int m_CursorPosition;
+        TElement m_CurrentItem;
+
+        /// <summary>
+        /// Occurs after the current item has changed.
+        /// </summary>
+        public event EventHandler<TElement> CurrentChanged;
+        /// <summary>
+        /// Occurs before the current item changes.
+        /// </summary>
+        public event CurrentChangingEventHandler CurrentChanging;
+
+        /// <summary>
+        /// Gets the current item in the view.
+        /// </summary>
+        public TElement CurrentItem
+        {
+            get
+            {
+                return m_CursorPosition > -1 && m_CursorPosition < this.Count ? this[m_CursorPosition] : default(TElement);
+            }
+            set
+            {
+                MoveCurrentTo(value);
+                m_CurrentItem = value;
+            }
+        }
+        /// <summary>
+        /// Gets the ordinal position of the current item in the view.
+        /// </summary>
+        public int CurrentPosition
+        {
+            get
+            {
+                return m_CursorPosition;
+            }
+            set
+            {
+                MoveCurrentToIndex(value);
+            }
+        }
+
+        public bool IsCurrentAfterLast
+        {
+            get
+            {
+                return m_CursorPosition >= base.Count; 
+            } 
+        }
+
+        public bool IsCurrentBeforeFirst
+        {
+            get
+            {
+                return m_CursorPosition < 0; 
+            } 
+        }
+
+        public bool MoveCurrentToFirst()
+        { 
+            return MoveCurrentToIndex(0);
+        }
+
+        public bool MoveCurrentToLast() 
+        { 
+            return MoveCurrentToIndex(base.Count - 1);
+        }
+
+        public bool MoveCurrentToNext() 
+        {
+            return MoveCurrentToIndex(m_CursorPosition + 1); 
+        }
+
+        public bool MoveCurrentToPosition(int index) 
+        { 
+            return MoveCurrentToIndex(index); 
+        }
+
+        public bool MoveCurrentToPrevious() 
+        {
+            return MoveCurrentToIndex(m_CursorPosition - 1); 
+        }
+
+        public bool MoveCurrentTo(TElement item) 
+        {
+            return !object.ReferenceEquals(m_CurrentItem,CurrentItem) ? MoveCurrentToIndex(IndexOf(item)) : true;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="CurrentChanging"/> event.
+        /// </summary>
+        protected virtual void OnCurrentChanging(CurrentChangingEventArgs e)
+        {
+            if (CurrentChanging != null)
+                CurrentChanging(this, e);
+        }
+
+        /// <summary>
+        /// Raises the <see cref="CurrentChanged"/> event.
+        /// </summary>
+        protected virtual void OnCurrentChanged(TElement e)
+        {
+            if (CurrentChanged != null)
+            {
+                CurrentChanged(this, e);
+                RaisePropertyChanged("CurrentItem");
+            }
+        }
+
+        // move the cursor to a new position
+        bool MoveCurrentToIndex(int index)
+        {
+            // invalid?
+            if (index < -1 || index >= base.Count)
+                return false;
+
+            // no change?
+            if (index == m_CursorPosition)
+                return false;
+
+            // fire changing
+            var e = new CurrentChangingEventArgs();
+            OnCurrentChanging(e);
+            if (e.Cancel)
+            {
+                return false;
+            }
+
+            // change and fire changed
+            m_CursorPosition = index;
+            OnCurrentChanged(CurrentItem);
+            return true;
+        }
+
+    }
+}
